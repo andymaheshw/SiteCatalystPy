0211e62e55c6f5dbcb85674ccf86fb96909f63a4
andy <andymaheshw@gmail.com>
2016-03-13 23:23:49 -0400

    updates to requests and private variables

--- adobeanalytics.py
+++ adobeanalytics.py
@@ -7,17 +7,17 @@ Created on Sun Mar 06 00:27:21 2016
 
 from datetime import date, timedelta
 from collections import defaultdict
-import urllib2, time, binascii, sha, json, urllib
+import requests, time, binascii, sha, json, urllib #adding hashlib
 import pandas as pd
 
 # Authentication
-class adobeanalytics:
+class AdobeAnalytics:
     # Define all variables
-    def __init__(self, user_name, shared_secret, company):
-        self.user_name = user_name
-        self.shared_secret = shared_secret
+    def __init__(self, user_name, shared_secret, company, endpoint='https://api.omniture.com/admin/1.4/rest/'):
+        self.__user_name = user_name
+        self.__shared_secret = shared_secret
         self.company = company
-        self.api_url = 'https://api.omniture.com/admin/1.4/rest/'
+        self.api_url = endpoint
     
     # Method call to get API Endpoint
     def get_endpoint(self):
@@ -26,23 +26,17 @@ class adobeanalytics:
              
     #Core Method, Actually runs the JSON request    
     def omni_api(self, json_values, endpoint):
-        req = urllib2.Request('%s?method=%s' % (self.api_url, endpoint), json.dumps(json_values))       
+        req = requests.get('%s?method=%s' % (self.api_url, endpoint), json.dumps(json_values))       
         nonce = str(time.time())
         base64nonce = binascii.b2a_base64(binascii.a2b_qp(nonce))
         created_date = time.strftime("%Y-%m-%dT%H:%M:%SZ",  time.gmtime())
-        sha_object = sha.new(nonce + created_date + '%s' % (self.shared_secret))
+        sha_object = sha.new(nonce + created_date + '%s' % (self.__shared_secret))
         password_64 = binascii.b2a_base64(sha_object.digest())
-        X_str = 'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"' % ('%s:%s' % (self.user_name, self.company), password_64.strip(), base64nonce.strip(), created_date)
+        X_str = 'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"' % ('%s:%s' % (self.__user_name, self.company), password_64.strip(), base64nonce.strip(), created_date)
         req.add_header('X-WSSE', X_str)
-        response = urllib2.urlopen(req)
-        the_page = json.loads(response.read())
+        response = req.json()
+        the_page = response
         return the_page
-    
-    #Report.Queue
-    def report_queue():
-    
-    #Report.Run
-    
-    
-    
-    
+     
+     def ReportRun():
+         print "in progress"
