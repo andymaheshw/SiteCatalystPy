a4e233eade452e98965fbcce018e21ac5d80e366
andy <andymaheshw@gmail.com>
2016-03-24 23:41:32 -0400

    fixed up adobe analytics

--- adobeanalytics.py
+++ adobeanalytics.py
@@ -7,7 +7,7 @@ Created on Sun Mar 06 00:27:21 2016
 
 from datetime import date, timedelta
 from collections import defaultdict
-import requests, time, binascii, sha, json, urllib #adding hashlib
+import requests, time, binascii, hashlib, json, urllib #adding hashlib
 import pandas as pd
 
 # Authentication
@@ -26,17 +26,29 @@ class AdobeAnalytics:
              
     #Core Method, Actually runs the JSON request    
     def omni_api(self, json_values, endpoint):
-        req = requests.get('%s?method=%s' % (self.api_url, endpoint), json.dumps(json_values))       
         nonce = str(time.time())
         base64nonce = binascii.b2a_base64(binascii.a2b_qp(nonce))
         created_date = time.strftime("%Y-%m-%dT%H:%M:%SZ",  time.gmtime())
-        sha_object = sha.new(nonce + created_date + '%s' % (self.__shared_secret))
+        sha_object = hashlib.sha1(nonce + created_date + '%s' % (self.__shared_secret)) 
         password_64 = binascii.b2a_base64(sha_object.digest())
         X_str = 'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"' % ('%s:%s' % (self.__user_name, self.company), password_64.strip(), base64nonce.strip(), created_date)
-        req.add_header('X-WSSE', X_str)
+        header = {'X-WSSE':X_str}
+        req = requests.get('%s?method=%s' % (self.api_url, endpoint), params=json.dumps(json_values), headers=header)
         response = req.json()
         the_page = response
         return the_page
      
-     def ReportRun():
-         print "in progress"
+    def ReportRun(self):
+         # build JSON
+        values = {}
+         #define endpoint
+        endpoint = 'Report.Run'
+        omni_api(values, endpoint)
+    
+    def CompanyGetReportSuites(self):
+         values = {}
+         endpoint = 'Company.GetReportSuites'
+         val = self.omni_api(values, endpoint)
+         print val
+         
+        
