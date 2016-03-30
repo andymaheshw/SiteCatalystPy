1f412ba99a8ce3239021ab1583a401952f860ba9
randyzwitch <rzwitch@gmail.com>
2016-03-30 10:02:52 -0400

    Update to proper endpoints

--- adobeanalytics.py
+++ adobeanalytics.py
@@ -64,46 +64,43 @@ class AdobeAnalytics:
         return self.__callapi('ReportSuite.GetActivation', rsid_list = rsid_list)
         
     def GetAxleStartDate(self):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetAxleStartDate', )
     
     def GetBaseCurrency(self, ):
-        return self.__callapi('Company.GetReportSuites', )
-    
-    def GetBaseCurrency(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetBaseCurrency', )
     
     def GetBaseURL(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetBaseURL', )
     
     def GetBookmarks(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Bookmark.GetBookmarks', )
     
     def GetCalculatedMetrics(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetCalculatedMetrics', )
     
-    def GetClassification(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+    def GetClassifications(self, ):
+        return self.__callapi('ReportSuite.GetClassifications', )
     
     def GetCustomCalendar(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetCustomCalendar', )
     
     def GetDashboards(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Bookmark.GetDashboards', )
     
     def GetDataWarehouseDisplay(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetDataWarehouseDisplay', )
     
     def GetDefaultPage(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetDefaultPage', )
     
     def GetDiscoverEnabled(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetDiscoverEnabled', )
     
     def GetEcommerce(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetEcommerce', )
 
     def GetElements(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Report.GetElements', )
       
     def GetEndpoint(self, company): 
         """ 
@@ -115,120 +112,117 @@ class AdobeAnalytics:
         return self.__callapi('Company.GetEndpoint', "GET", company = company)
     
     def GetEvars(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetEvars', )
     
     def GetFeed(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('DataFeed.GetFeed', )
     
     def GetFeeds(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('DataFeed.GetFeeds', )
     
     def GetFunctions(self, ):
-        return self.__callapi('Company.GetReportSuites', )
-    
-    def GetFunctions(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('CalculatedMetrics.GetFunctions', )
     
     def GetGeoSegmentation(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetGeoSegmentation', )
     
     def GetGroups(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Permissions.GetGroups', )
     
     def GetInternalURLFilters(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetInternalURLFilters', )
 
     def GetIPAddressExclusions(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetIPAddressExclusions', )
 
     def GetIPObfuscation(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetIPObfuscation', )
     
     def GetKeyVisitors(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetKeyVisitors', )
     
     def GetListVariables(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetListVariables', )
     
     def GetLocalization(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetLocalization', )
     
     def GetLogin(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Permissions.GetLogin', )
     
     def GetLogins(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Permissions.GetLogins', )
     
     def GetMarketingChannelExpiration(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetMarketingChannelExpiration', )
     
     def GetMarketingChannelRules(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetMarketingChannelRules', )
     
     def GetMarketingChannels(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetMarketingChannels', )
     
     def GetMetrics(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Report.GetMetrics', )
     
     def GetMobileAppReporting(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetMobileAppReporting', )
     
     def GetPaidSearchDetection(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetPaidSearchDetection', )
     
     def GetPrivacySettings(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetPrivacySettings', )
     
     def GetProps(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetProps', )
     
     def GetQueue(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Report.GetQueue', )
     
     def GetRealTimeReport(self, ):
         return self.__callapi('Company.GetReportSuites', )
     
     def GetRealTimeSettings(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetRealTimeSettings', )
     
     def GetReportDescription(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Bookmark.GetReportDescription', )
       
     def GetReportSuites(self):
         """Returns all report suites available to user from a given company."""
         return self.__callapi('Company.GetReportSuites')
     
     def GetScheduledSpike(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetScheduledSpike', )
 
     def GetSegments(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetSegments', )
     
     def GetSiteTitle(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetSiteTitle', )
     
-    def GetSuccessEvents(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+    def GetEvents(self, ):
+        return self.__callapi('ReportSuite.GetEvents', )
     
     def GetTemplate(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetTemplate', )
     
     def GetTimeZone(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetTimeZone', )
     
     def GetTrackingServer(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Company.GetTrackingServer', )
     
     def GetTransactionEnabled(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetTransactionEnabled', )
     
     def GetUniqueVisitorVariable(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetUniqueVisitorVariable', )
     
     def GetVersionAccess(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('Company.GetVersionAccess', )
     
     def GetVideoSettings(self, ):
-        return self.__callapi('Company.GetReportSuites', )
+        return self.__callapi('ReportSuite.GetVideoSettings', )
 
