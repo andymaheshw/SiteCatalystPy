1788bd52e0e2764ed4d439c62c4ae24c8e276b09
randyzwitch <rzwitch@gmail.com>
2016-08-12 10:29:35 -0400

    See if this allows pip installs

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+classifiers = (
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'License :: OSI Approved :: MIT License',
+)
+
+required = ('pandas', 'python-dateutil', 'pytz', 'requests', 'six')
+
+kw = {
+    'name': 'AdobeAnalytics',
+    'version': '0.1.0',
+    'description': 'A Python wrapper for the Adobe Analytics API',
+    'author': 'Andy Maheshwari',
+    'author_email': 'andymaheshw@gmail.com',
+    'license': 'MIT License',
+    'url': 'https://github.com/andymaheshw/AdobeAnalytics',
+    'keywords': 'digital analytics',
+    'install_requires': required,
+    'zip_safe': True,
+}
+
+setup(**kw)
