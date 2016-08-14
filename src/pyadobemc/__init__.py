68900ae889bb4964cf8a112eccac0cc39e138dd4
randyzwitch <rzwitch@gmail.com>
2016-08-13 21:32:55 -0400

    Guess again

--- src/pyadobemc/__init__.py
+++ src/pyadobemc/__init__.py
@@ -4,3 +4,8 @@ Created on Sun Mar 13 23:26:38 2016
 
 @author: anirudhmaheshwari
 """
+__version__ = "0.1.0"
+
+__all__ = ["AdobeAnalytics"]
+
+from .pyadobemc import AdobeAnalytics
--- tests/test_pyadobemc.py
+++ tests/test_pyadobemc.py
@@ -1,15 +1,8 @@
+import os
 import pyadobemc
 
 
 def test_main():
     assert pyadobemc  # use your library here
-
-
-# import os
-# from AdobeAnalytics import adobeanalytics
-
-# #aa = adobeanalytics.AdobeAnalytics(user_name=os.environ['USER'], shared_secret=os.environ['SECRET'], debug = True)
-
-# #Test 1
-# def test_answer():
-# 	assert isinstance(aa.GetActivation(rsid_list = "zwitchdev"), list) == True
+	aa = pyadobemc.AdobeAnalytics(user_name=os.environ['USER'], shared_secret=os.environ['SECRET'], debug = True)
+	assert isinstance(aa.GetActivation(rsid_list = "zwitchdev"), list) == True
