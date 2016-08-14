d6c149035ecf9dfd8ffcd297a171bab1d9edffde
randyzwitch <rzwitch@gmail.com>
2016-08-13 20:31:41 -0400

    Update setup with cookiecutter setup file
    Using cookiecutter-python package, generate setup. See if it builds.

---
+++ tests/test_pyadobemc.py
@@ -0,0 +1,15 @@
+import pyadobemc
+
+
+def test_main():
+    assert pyadobemc  # use your library here
+
+
+# import os
+# from AdobeAnalytics import adobeanalytics
+
+# #aa = adobeanalytics.AdobeAnalytics(user_name=os.environ['USER'], shared_secret=os.environ['SECRET'], debug = True)
+
+# #Test 1
+# def test_answer():
+# 	assert isinstance(aa.GetActivation(rsid_list = "zwitchdev"), list) == True
