cbda8bdeb202a6294020b475e4084be82baf0937
randyzwitch <rzwitch@gmail.com>
2016-08-13 21:46:47 -0400

    Fix tabs vs. spaces

--- tests/test_pyadobemc.py
+++ tests/test_pyadobemc.py
@@ -3,6 +3,6 @@ import pyadobemc
 
 
 def test_main():
-    assert pyadobemc  # use your library here
+	assert pyadobemc  # use your library here
 	aa = pyadobemc.AdobeAnalytics(user_name=os.environ['USER'], shared_secret=os.environ['SECRET'], debug = True)
 	assert isinstance(aa.GetActivation(rsid_list = "zwitchdev"), list) == True
